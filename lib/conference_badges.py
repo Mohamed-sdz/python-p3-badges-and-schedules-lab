042da8542f7fbb9f39891954768772f8c7c6ea0f
saiderrahmani <didi.mousedidi@outlook.com>
2023-08-07 22:40:50 -0400

    update lib

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(speakers):
+    return [badge_maker(name) for name in speakers]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for idx, name in enumerate(speakers, start=1):
+        room_assignments.append("Hello, {}! You'll be assigned to room {}!".format(name, idx))
+    return room_assignments
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    room_assignments = assign_rooms(speakers)
+    
+    for badge in badges:
+        print(badge)
+    for assignment in room_assignments:
+        print(assignment)
+
+ 
